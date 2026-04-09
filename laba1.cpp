9173c4b8e336c0ff0dfa50c34e89a5e45e270610
Dima Chinin <dmitrychinch@gmail.com>
2026-04-10 00:17:17 +0700

    Добавлены подсказки для пользователя и проверка на положительные

--- laba1.cpp
+++ laba1.cpp
@@ -12,14 +12,20 @@ void calcTrap(double a, double b, double h, double &per, double &sq, double &mid
 int main() {
     setlocale(LC_ALL, "Russian");
     double a, b, h, per, sq, mid;
+    
+    cout << "Введите основания трапеции и ее высоту:";
 
     cin >> a >> b >> h;
     
+    if (a < 0 or b < 0 or h < 0) {
+      cout << "Необходимо ввести положительные значения!";
+    }
+    
     calcTrap(a, b, c, per, sq, mid);
     //ama rok star
-    cout << per << endl; 
-    cout << sq << endl; 
-    cout << mid << endl; 
+    cout << "Периметр трапеции:" << per << endl; 
+    cout << "Площадь трапеции:" << sq << endl; 
+    cout << "Средняя линия трапеции:" << mid << endl; 
 
     return 0;
 }
